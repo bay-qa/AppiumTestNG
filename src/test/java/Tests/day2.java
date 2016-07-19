36aa6ba504dc83dfcb68b2d290bf283ab797c1a4
ab054 <alfamiliya@gmail.com>
2016-07-19 11:41:48 -0700

    day2

--- src/test/java/Tests/day2.java
+++ src/test/java/Tests/day2.java
@@ -1,7 +1,34 @@
 package Tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 /**
  * Created by abarabash on 7/19/16.
  */
-public class day2 {
+public class day2 extends TestBase{
+
+    @Test
+    public void test001() {
+
+        Assert.assertTrue(scr.mainScreen.searchBar.isDisplayed());
+
+        scr.mainScreen.searchBar.click();
+
+        driver.findElement(By.id("search_src_text")).sendKeys("Metallica");
+
+        List<WebElement> listResult = driver.findElements(By.id("page_list_item_title"));
+
+        Assert.assertTrue(listResult.get(0).getText().contains("Metallica"));
+
+        listResult.get(0).click();
+
+    }
+
+
+
 }
