109b77afbaa83e7a81d29f427469e2c4785b8892
ab054 <alfamiliya@gmail.com>
2016-07-25 18:44:52 -0700

    day6_completed

--- src/test/java/Tests/Day6.java
+++ src/test/java/Tests/Day6.java
@@ -1,19 +1,65 @@
 package Tests;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
 
 /**
  * Created by abarabash on 7/25/16.
  */
-public class Day6 extends TestBase {
+public class Day6 {
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    AppiumDriver driver;
+    String URL;
+
+    @Parameters("Device_ID")
+    @BeforeTest
+    public void startDriver(String deviceId) throws Exception {
+
+        setCapabilities(deviceId);
+
+        URL = "http://127.0.0.1:4444/wd/hub";
+        driver = new AndroidDriver(new URL(URL), capabilities);
+        driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+    }
+
+    public void setCapabilities(String device) {
+
+        String appPath = System.getProperty("user.dir") + "/apps/android/app-alpha-debug.apk";
+
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,
+                "Chrome");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,
+                device);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,
+                "Android");
+        capabilities.setCapability(MobileCapabilityType.APP_PACKAGE,
+                "com.android.chrome");
+        capabilities.setCapability(MobileCapabilityType.UDID, device);
+    }
 
     @Test
-    public void test001() {
+    public void testForParallel() throws Exception {
 
-        driver.get("https://codeshare.io/zO1Sk");
+        driver.get("http://the-internet.herokuapp.com/nested_frames");
 
-        //driver2.get("https://codeshare.io/zO1Sk");
+        driver.switchTo().frame("frame-top");
 
+        driver.switchTo().frame("frame-middle");
+
+        assertEquals(driver.findElement((By.id("content"))).getText(), "MIDDLE");
 
     }
 }
--- src/test/java/Utils/AppiumServer.java
+++ src/test/java/Utils/AppiumServer.java
@@ -7,6 +7,8 @@ public class AppiumServer {
 
     public static void startAppiumServer(String mainAppiumPort, String bootstrapPort, String deviceId) throws Exception {
 
+        //Runtime.getRuntime().exec("cd /Users/abarabash/node_modules/appium");
+
         Runtime.getRuntime().exec("appium -p " + mainAppiumPort + " -bp " + bootstrapPort + " -U " + deviceId + " -a 127.0.0.1");
         Thread.sleep(7000);
 
